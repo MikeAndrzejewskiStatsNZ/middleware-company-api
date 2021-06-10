03e4c312dcdca3242bbfcc3797cee15302a0e542
Michael Andrzejewski <mike.andrzejewski@stats.govt.nz>
2021-06-11 09:59:47 +1200

    adding reduirect route and homepage route

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # middleware-company-api
+
+### Setup Instructions
+clone down the repo
+npm install
+
+Notes:
+
+I have added extra comments for some explanations, makes the code look a bit messy.
+
+For "production hosting" i went with Google Cloud Services, here is the URL.
+
+Rename .example-env to .env.
--- src/app.js
+++ src/app.js
@@ -3,10 +3,20 @@ Created by: Michael Andrzejewski, June 2021
 */
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const companiesRoute = require('./routes/v1/companies/companies')
 
 // Nothing really specified about versioning or how many other routes there could be, so i made this pretty rudimentary.
 app.use('/v1/companies', companiesRoute)
+
+// Holding text for the home page
+app.get('/', (req, res) => {
+  res.send('Welcome to the middleware api test')
+})
+
+// Just redirecting if the routes are note found
+app.all('*', function (req, res) {
+  res.redirect('/')
+})
 
 app.listen(port, () => console.log(`Node server running on port: ${port}`))
